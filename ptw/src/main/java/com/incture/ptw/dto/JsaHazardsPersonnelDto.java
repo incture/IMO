ac84aa0c16528b0eac75ef28599dadc9d02278ce
inc01389 <krishna.s@incture.com>
2021-04-07 15:32:30 +0530

    dto commit

--- ptw/src/main/java/com/incture/ptw/dto/JsaHazardsPersonnelDto.java
+++ ptw/src/main/java/com/incture/ptw/dto/JsaHazardsPersonnelDto.java
@@ -1,5 +1,17 @@
 package com.incture.ptw.dto;
 
+import lombok.Data;
+
+@Data
 public class JsaHazardsPersonnelDto {
+	private Integer permitNumber;
+	private Integer personnel;
+	private Integer performInduction;
+	private Integer mentorCoachSupervise;
+	private Integer verifyCompetencies;
+	private Integer addressLimitations;
+	private Integer manageLanguageBarriers;
+	private Integer wearSeatBelts;
+	
 
 }
