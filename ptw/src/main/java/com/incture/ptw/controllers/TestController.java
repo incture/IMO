a8cb2f4ea7655a6da0924b11e9a1be503a1ce9a8
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-08 19:44:49 +0530

    testcontroller updated

--- ptw/src/main/java/com/incture/ptw/controllers/TestController.java
+++ ptw/src/main/java/com/incture/ptw/controllers/TestController.java
@@ -1,8 +1,12 @@
 package com.incture.ptw.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.incture.ptw.dto.CreateRequestDto;
 
 @RestController
 @RequestMapping("/test")
@@ -12,5 +16,10 @@ public class TestController {
 	public String test()
 	{
 		return "Hello";
+	}
+	@PostMapping("/forDto")
+	public void get(@RequestBody CreateRequestDto createRequestDto)
+	{
+		System.out.println(createRequestDto);
 	}
 }
