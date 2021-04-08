3cc21de20eab378cfa448d9db3bfc3edcd1e2e78
inc01389 <krishna.s@incture.com>
2021-04-08 14:05:57 +0530

    first dao

--- ptw/src/main/java/com/incture/ptw/dao/JsaHeaderDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaHeaderDao.java
@@ -9,9 +9,17 @@ import com.incture.ptw.dto.JsaheaderDto;
 @Repository
 public class JsaHeaderDao extends BaseDao {
 	private void insertJsaHeader(JsaheaderDto jsaheaderDto){
-		Query query = getSession().createNativeQuery("INSERT INTO \"IOP\".\"JSAREVIEW\" VALUES (?,?,?,?,?,?,?)");
-		//query.setParameter(arg0, arg1)
-		
+		Query query = getSession().createNativeQuery("INSERT INTO \"IOP\".\"JSAHEADER\" VALUES (?,?,?,?,?,?,?,?,?)");
+		query.setParameter(1, jsaheaderDto.getPermitNumber());
+		query.setParameter(2, jsaheaderDto.getJsaPermitNumber());
+		query.setParameter(3, jsaheaderDto.getHasCwp());
+		query.setParameter(4, jsaheaderDto.getHasHwp());
+		query.setParameter(5, jsaheaderDto.getHasCse());
+		query.setParameter(6, jsaheaderDto.getTaskDescription());
+		query.setParameter(7, jsaheaderDto.getIdentifyMostSeriousPotentialInjury());
+		query.setParameter(8, jsaheaderDto.getIsActive());
+		query.setParameter(9, jsaheaderDto.getStatus());
+		query.executeUpdate();		
 	} 
 
 }
