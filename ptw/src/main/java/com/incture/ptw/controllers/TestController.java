d20e09005664bee40fcb0c28bc1cc5f676807a3c
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-08 18:54:06 +0530

    testcontroller added

---
+++ ptw/src/main/java/com/incture/ptw/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.incture.ptw.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/get")
+	public String test()
+	{
+		return "Hello";
+	}
+}
