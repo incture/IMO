f0d52bc8c0b91ee03bf320746bee6d4ffa23168c
Nischal.Jadhav <Nischal.Jadhav@INC01394>
2021-04-08 10:52:47 +0530

    Nischal -- CreateRequestDto file is added

---
+++ ptw/src/main/java/com/incture/ptw/dto/CreateRequestDto.java
@@ -0,0 +1,45 @@
+package com.incture.ptw.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateRequestDto {
+	private JsaHazardsMobileDto jsaHazardsMobileDto;
+	private PtwCwpWorkTypeDto ptwCwpWorkTypeDto;
+	private JsaHazardsExcavationdDto jsaHazardsExcavationdDto;
+	private JsaHazardsDroppedDto jsaHazardsDroppedDto;
+	private JsaHazardsPressurizedDto jsaHazardsPressurizedDto;
+	private JsaLocationDto jsaLocationDto;
+	private PtwCloseOutDto ptwCloseOutDto;
+	private JsaHazardsVisibilityDto jsaHazardsVisibilityDto;
+	private JsaHazardsPersonnelDto jsaHazardsPersonnelDto;
+	private JsaHazardsHighNoiseDto jsaHazardsHighNoiseDto;
+	private JsaReviewDto jsaReviewDto;
+	private JsaHazardsMovingDto jsaHazardsMovingDto;
+	private JsaHazardsVoltageDto jsaHazardsVoltageDto;
+	private JsaHazardsLiftingDto jsaHazardsLiftingDto;
+	private JsaStepsDto jsaStepsDto;
+	private JsaHazardsManualDto jsaHazardsManualDto;
+	private JsaHazardsSpillsDto jsaHazardsSpillsDto;
+	private JsaStopTriggerDto jsaStopTriggerDto;
+	private JsappeDto jsappeDto;
+	private JsaHazardsHeightsDto jsaHazardsHeightsDto;
+	private JsaHazardsToolsDto jsaHazardsToolsDto;
+	private PtwHwpWorkTypeDto ptwHwpWorkTypeDto;
+	private JsaheaderDto jsaheaderDto;
+	private PtwCseWorkTypeDto ptwCseWorkTypeDto;
+	private JsaHazardsIgnitionDto jsaHazardsIgnitionDto;
+	private PtwTestResultsDto ptwTestResultsDto;
+	private JsaHazardscseDto jsaHazardscseDto;
+	private JsaHazardsWeatherDto jsaHazardsWeatherDto;
+	private JsaRiskAssesmentDto jsaRiskAssesmentDto;
+	private PtwApprovalDto ptwApprovalDto;
+	private PtwPeopleDto ptwPeopleDto;
+	private JsaHazardsSubstancesDto jsaHazardsSubstancesDto;
+	private JsaHazardsElectricalDto jsaHazardsElectricalDto;
+	private JsaHazardsFallsDto jsaHazardsFallsDto;
+	private PtwHeaderDto ptwHeaderDto;
+	private PtwRequiredDocumentDto ptwRequiredDocumentDto;
+	private PtwTestRecordDto ptwTestRecordDto;
+	private JsaHazardsSimultaneousDto jsaHazardsSimultaneousDto;
+}
