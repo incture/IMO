99540ff5638ccafb13b8a9cee168561a24f95970
inc01389 <krishna.s@incture.com>
2021-04-08 11:01:03 +0530

    dao package

---
+++ ptw/src/main/java/com/incture/ptw/dao/JsaHeaderDao.java
@@ -0,0 +1,10 @@
+package com.incture.ptw.dao;
+
+import com.incture.ptw.dto.JsaheaderDto;
+
+public class JsaHeaderDao {
+	private void insertJsaHeader(JsaheaderDto jsaheaderDto){
+		Query 
+	}
+
+}
