b7bd871780ff3b8c6f20f8007fd6fd88a16ceeca
inc01389 <krishna.s@incture.com>
2021-04-09 14:25:29 +0530

    NEW

--- ptw/src/main/java/com/incture/ptw/dao/GetJsaDetailsDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/GetJsaDetailsDao.java
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Repository;
 public class GetJsaDetailsDao extends BaseDao {
 	public List<Object[]> downloaadData() {
 		Query query = getSession().createNativeQuery(
-				"select J.*,L.FACILTYORSITE, R.* from IOP.JSAHEADER as J inner join IOP.JSA_LOCATION as L on J.PERMITNUMBER=L.PERMITNUMBER"
+				"select J.*,L.FACILTYORSITE, R.CREATEDBY,R.APPROVEDBY,R.APPROVEDDATE,R.LASTUPDATEDBY,R.LASTUPDATEDDATE,R.CREATEDDATE"
+						+ " from IOP.JSAHEADER as J inner join IOP.JSA_LOCATION as L on J.PERMITNUMBER=L.PERMITNUMBER"
 						+ " inner join IOP.JSAREVIEW as R on J.PERMITNUMBER=R.PERMITNUMBER");
 		List<Object[]> res = query.getResultList();
+
 		System.out.println(res.toString());
 		// for(Object[] row : res){
 		// JsaheaderDto jsaheaderDto = new JsaheaderDto();
