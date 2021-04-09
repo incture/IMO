c7586f6540f9e561f3e1ef44869abcf9e66cde14
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-09 18:18:08 +0530

    jsabylocationdao added

---
+++ ptw/src/main/java/com/incture/ptw/dao/JsaByLocationDao.java
@@ -0,0 +1,18 @@
+package com.incture.ptw.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.incture.ptw.dto.JsaDetailsDto;
+
+@Repository("GetJsaByLocationDao")
+public class JsaByLocationDao {
+	public List<String> getPermitNumberList(String mowi, String facility) {
+		return null;
+	}
+
+	public List<JsaDetailsDto> getJsaByLocation(String mowi, String facility) {
+		return null;
+	}
+}
