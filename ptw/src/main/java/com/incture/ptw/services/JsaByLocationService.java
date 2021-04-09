7df4dd2b415c689b815e670052e5584c88c9dc20
inc01389 <krishna.s@incture.com>
2021-04-09 18:32:12 +0530

    jsabylocation controller and service

---
+++ ptw/src/main/java/com/incture/ptw/services/JsaByLocationService.java
@@ -0,0 +1,51 @@
+package com.incture.ptw.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.incture.ptw.dao.GetJsaDetailsDao;
+import com.incture.ptw.dao.JsaByLocationDao;
+import com.incture.ptw.dto.JsaDetailsDto;
+import com.incture.ptw.util.ResponseDto;
+
+@Service
+@Transactional
+public class JsaByLocationService {
+
+	@Autowired
+	private JsaByLocationDao jsaByLocationDao;
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public ResponseDto getJsaByLocation(String muwi, String facility) {
+		logger.info("DownloadDataService ");
+
+		ResponseDto responseDto = new ResponseDto();
+		responseDto.setStatus(Boolean.TRUE);
+		responseDto.setStatusCode(200);
+		try {
+			responseDto.setData(jsaByLocationDao.getJsaByLocation(muwi, facility));
+			responseDto.setMessage("Data displayed successfully");
+
+		} catch (Exception e) {
+
+			logger.error("DownloadDataService " + e.getMessage());
+			responseDto.setStatus(Boolean.FALSE);
+			responseDto.setStatusCode(500);
+			responseDto.setMessage(e.getMessage());
+
+		}
+
+		logger.info("DownloadDataService " + responseDto);
+
+		return responseDto;
+
+	}
+
+}
