c25b843cac8749dec67c2d22838121b7614a6c17
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-12 20:04:22 +0530

    updated

--- ptw/src/main/java/com/incture/ptw/dao/JsaByLocationDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaByLocationDao.java
@@ -1,10 +1,14 @@
 package com.incture.ptw.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
+
+import com.incture.ptw.dto.JsaDetailsDto;
+import com.incture.ptw.dto.JsaRecord;
 
 @Repository("GetJsaByLocationDao")
 public class JsaByLocationDao extends BaseDao {
@@ -36,7 +40,7 @@ public class JsaByLocationDao extends BaseDao {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Object> getJsaByLocation(String muwi, String facility) {
+	public List<JsaDetailsDto> getJsaByLocation(String muwi, String facility) {
 		List<String> permitNumberList = getPermitNumberList(muwi, facility);
 		String sql = "select J.JSAPERMITNUMBER, J.TASKDESCRIPTION,J.STATUS,P.PTWPERMITNUMBER, "
 				+ "R.CREATEDDATE,R.CREATEDBY , L.FACILTYORSITE, R.LASTUPDATEDDATE, "
@@ -47,6 +51,45 @@ public class JsaByLocationDao extends BaseDao {
 				+ "where J.PERMITNUMBER IN (:list) ORDER BY R.LASTUPDATEDDATE DESC ";
 		Query q = getSession().createNativeQuery(sql);
 		q.setParameter("list", permitNumberList);
-		return q.getResultList();
+		List<Object> obj = q.getResultList();
+
+		List<JsaRecord> jsaList = null;
+		int objLength = obj.size();
+		for (int i = 0; i < objLength; i++) {
+			List<Object> rs = (List<Object>) obj.get(i);
+			JsaRecord jsaDto = new JsaRecord();
+			jsaDto.setJsaPermitNumber((String) rs.get(1));
+			jsaDto.setTaskDescription((String) rs.get(2));
+			jsaDto.setStatus((String) rs.get(3));
+			jsaDto.setPtwPermitNumber((String) rs.get(4));
+			jsaDto.setCreatedDate((Date) rs.get(5));
+			jsaDto.setCreatedBy((String) rs.get(6));
+			jsaDto.setFacilityOrSite((String) rs.get(7));
+			jsaDto.setLastUpdatedDate((Date) rs.get(8));
+			jsaDto.setApprovedDate((Date) rs.get(9));
+			jsaDto.setPermitNumber((Integer) rs.get(10));
+			jsaList.add(jsaDto);
+		}
+		List<JsaDetailsDto> jsaDetailsDtoList = null;
+		for (JsaRecord jDto : jsaList) {
+			JsaDetailsDto temp = new JsaDetailsDto();
+			// if(jsaDetailsDtoList.contains(jDto.getPermitNumber()))
+			// {
+			// int index=Collections.binarySearch(jsaDetailsDtoList,
+			// jDto.getPermitNumber());
+			// temp=jsaDetailsDtoList.get(index);
+			// temp.getPtwPermitNumber().add(jDto.getPtwPermitNumber());
+			// continue;
+			// }
+			// else
+			// {
+			// temp.setPermitNumber(jDto.getPermitNumber());
+			// }
+			temp.setApprovedDate(jDto.getApprovedDate());
+			temp.setCreatedBy(jDto.getCreatedBy());
+
+			jsaDetailsDtoList.add(temp);
+		}
+		return null;
 	}
 }
---
+++ ptw/src/main/java/com/incture/ptw/dto/JsaRecord.java
@@ -0,0 +1,19 @@
+package com.incture.ptw.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class JsaRecord {
+	private Date approvedDate;
+	private String createdBy;
+	private Date createdDate;
+	private String facilityOrSite;
+	private String jsaPermitNumber;
+	private Date lastUpdatedDate;
+	private int permitNumber;
+	private String ptwPermitNumber;
+	private String status;
+	private String taskDescription;
+}
