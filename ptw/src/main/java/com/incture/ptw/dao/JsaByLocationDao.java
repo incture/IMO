81364e683ca7178d7dd6e43b21659f2559e942e8
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-13 18:10:22 +0530

    updated

--- ptw/src/main/java/com/incture/ptw/dao/JsaByLocationDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaByLocationDao.java
@@ -53,25 +53,27 @@ public class JsaByLocationDao extends BaseDao {
 				+ "where J.PERMITNUMBER IN (:list) ORDER BY R.LASTUPDATEDDATE DESC ";
 		Query q = getSession().createNativeQuery(sql);
 		q.setParameter("list", permitNumberList);
-		List<Object> obj = q.getResultList();
+		System.out.println(q.getResultList().toArray());
+		List<Object[]> obj = q.getResultList();
 		logger.info(obj.toString());
 		List<JsaRecord> jsaList = null;
 		int objLength = obj.size();
 		for (int i = 0; i < objLength; i++) {
-			List<Object> rs = (List<Object>) obj.get(i);
+			Object[] rs = obj.get(i);
 			JsaRecord jsaDto = new JsaRecord();
-			jsaDto.setJsaPermitNumber((String) rs.get(1));
-			jsaDto.setTaskDescription((String) rs.get(2));
-			jsaDto.setStatus((String) rs.get(3));
-			jsaDto.setPtwPermitNumber((String) rs.get(4));
-			jsaDto.setCreatedDate((Date) rs.get(5));
-			jsaDto.setCreatedBy((String) rs.get(6));
-			jsaDto.setFacilityOrSite((String) rs.get(7));
-			jsaDto.setLastUpdatedDate((Date) rs.get(8));
-			jsaDto.setApprovedDate((Date) rs.get(9));
-			jsaDto.setPermitNumber((Integer) rs.get(10));
+			jsaDto.setJsaPermitNumber((String) rs[1]);
+			jsaDto.setTaskDescription((String) rs[2]);
+			jsaDto.setStatus((String) rs[3]);
+			jsaDto.setPtwPermitNumber((String) rs[4]);
+			jsaDto.setCreatedDate(null);
+			jsaDto.setCreatedBy((String) rs[6]);
+			jsaDto.setFacilityOrSite((String) rs[7]);
+			jsaDto.setLastUpdatedDate(null);
+			jsaDto.setApprovedDate(null);
+			jsaDto.setPermitNumber((Integer) rs[10]);
 			jsaList.add(jsaDto);
 		}
+		System.out.println(jsaList.toString());
 		logger.info(jsaList.toString());
 		List<JsaDetailsDto> jsaDetailsDtoList = null;
 		for (JsaRecord jDto : jsaList) {
@@ -97,6 +99,53 @@ public class JsaByLocationDao extends BaseDao {
 			jsaDetailsDtoList.add(temp);
 		}
 		logger.info(jsaDetailsDtoList.toString());
+		System.out.println(jsaDetailsDtoList.toString());
 		return jsaDetailsDtoList;
 	}
+	/*
+	 * @SuppressWarnings("unchecked") public List<JsaDetailsDto>
+	 * getJsaByLocations(String muwi, String facility) { List<String>
+	 * permitNumberList = getPermitNumberList(muwi, facility); String sql =
+	 * "select J.JSAPERMITNUMBER, J.TASKDESCRIPTION,J.STATUS,P.PTWPERMITNUMBER, "
+	 * + "R.CREATEDDATE,R.CREATEDBY , L.FACILTYORSITE, R.LASTUPDATEDDATE, " +
+	 * "R.APPROVEDDATE,J.PERMITNUMBER from IOP.JSA_LOCATION as L inner join " +
+	 * "IOP.JSAHEADER as J on L.PERMITNUMBER = J.PERMITNUMBER " +
+	 * "left join IOP.PTWHEADER as P on L.PERMITNUMBER = P.PERMITNUMBER " +
+	 * "inner join IOP.JSAREVIEW as R on L.PERMITNUMBER = R.PERMITNUMBER " +
+	 * "where J.PERMITNUMBER IN (?) ORDER BY R.LASTUPDATEDDATE DESC ";
+	 * Connection connection = ((SessionImpl) getSession()).connection();
+	 * PreparedStatement ps=connection.prepareStatement(sql); ps.setObject(1,)
+	 * ResultSet rs=ps.executeQuery(); List<Object> obj = q.getResultList();
+	 * logger.info(obj.toString()); List<JsaRecord> jsaList = null; int
+	 * objLength = obj.size(); for (int i = 0; i < objLength; i++) {
+	 * List<Object> rs = (List<Object>) obj.get(i); JsaRecord jsaDto = new
+	 * JsaRecord(); jsaDto.setJsaPermitNumber((String) rs.get(1));
+	 * jsaDto.setTaskDescription((String) rs.get(2)); jsaDto.setStatus((String)
+	 * rs.get(3)); jsaDto.setPtwPermitNumber((String) rs.get(4));
+	 * jsaDto.setCreatedDate((Date) rs.get(5)); jsaDto.setCreatedBy((String)
+	 * rs.get(6)); jsaDto.setFacilityOrSite((String) rs.get(7));
+	 * jsaDto.setLastUpdatedDate((Date) rs.get(8));
+	 * jsaDto.setApprovedDate((Date) rs.get(9));
+	 * jsaDto.setPermitNumber((Integer) rs.get(10)); jsaList.add(jsaDto); }
+	 * logger.info(jsaList.toString()); List<JsaDetailsDto> jsaDetailsDtoList =
+	 * null; for (JsaRecord jDto : jsaList) { JsaDetailsDto temp = new
+	 * JsaDetailsDto(); List<JsaDetailsDto> res = jsaDetailsDtoList.stream()
+	 * .filter(c ->
+	 * c.getJsaPermitNumber().equalsIgnoreCase(jDto.getJsaPermitNumber()))
+	 * .collect(Collectors.toList()); if
+	 * (res.get(0).getJsaPermitNumber().equals((jDto.getJsaPermitNumber()))) {
+	 * res.get(0).getPtwPermitNumber().add(jDto.getPtwPermitNumber()); continue;
+	 * } else { temp.setPermitNumber(jDto.getPermitNumber()); }
+	 * temp.setApprovedDate(jDto.getApprovedDate());
+	 * temp.setCreatedBy(jDto.getCreatedBy());
+	 * temp.setCreatedDate(jDto.getCreatedDate());
+	 * temp.setFacilityOrSite(Arrays.asList(jDto.getFacilityOrSite()));
+	 * temp.setJsaPermitNumber(jDto.getJsaPermitNumber());
+	 * temp.setLastUpdatedDate(jDto.getLastUpdatedDate());
+	 * temp.setPermitNumber(jDto.getPermitNumber());
+	 * temp.setStatus(jDto.getStatus());
+	 * temp.setTaskDescription(jDto.getTaskDescription());
+	 * jsaDetailsDtoList.add(temp); } logger.info(jsaDetailsDtoList.toString());
+	 * return jsaDetailsDtoList; }
+	 */
 }
