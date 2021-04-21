d74e676ef4d636c9f1355be6fbf21b922e55d233
inc01389 <krishna.s@incture.com>
2021-04-21 14:51:37 +0530

    key generator

--- ptw/src/main/java/com/incture/ptw/dao/JsaLocationDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaLocationDao.java
@@ -1,8 +1,31 @@
 package com.incture.ptw.dao;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
+
+import com.incture.ptw.dto.JsaLocationDto;
+import com.incture.ptw.dto.PtwHeaderDto;
 
 @Repository
 public class JsaLocationDao extends BaseDao{
+	public void insertJsaLocation(JsaLocationDto jsaLocationDto){
+		Query query = getSession().createNativeQuery("INSERT INTO \"IOP\".\"JSA_LOCATION\" VALUES (?,?,?,?,?,?)");
+//		query.setParameter(1, ptwHeaderDto.getPermitNumber());
+//		query.setParameter(2, ptwHeaderDto.getPtwPermitNumber());
+//		query.setParameter(3, ptwHeaderDto.getIsCwp());
+//		query.setParameter(4, ptwHeaderDto.getIsHwp());
+//		query.setParameter(5, ptwHeaderDto.getIsCse());
+//		query.setParameter(6, ptwHeaderDto.getPlannedDateTime());
+//		query.setParameter(7, ptwHeaderDto.getLocation());
+//		query.setParameter(8, ptwHeaderDto.getCreatedBy());
+//		query.setParameter(9, ptwHeaderDto.getContractorPerformingWork());
+//		query.setParameter(10, ptwHeaderDto.getEstimatedTimeOfCompletion());
+//		query.setParameter(11, ptwHeaderDto.getEquipmentId());
+//		query.setParameter(12, ptwHeaderDto.getWorkOrderNumber());
+//		query.setParameter(13, ptwHeaderDto.getStatus());
+//		query.executeUpdate();	
+	}
+
 
 }
--- ptw/src/main/java/com/incture/ptw/dao/KeyGeneratorDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/KeyGeneratorDao.java
@@ -8,11 +8,119 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class KeyGeneratorDao extends BaseDao {
-	public BigInteger getSerialNo() {
+	public BigInteger getPtwApprovalSerialNo() {
 		try {
 			String sql2 = "select IOP.PTWAPPROVAL_SEQ.NEXTVAL FROM DUMMY";
 			Query q2 = getSession().createNativeQuery(sql2);
 			return (BigInteger) q2.getSingleResult();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getPermitNumber() {
+		try {
+			String sql = "select IOP.PERMITNUMBER_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	public String getSerialNo() {
+		try {
+			String sql = "select IOP.SERIALNO_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return  q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	public String getJSASTEPSSerialNo() {
+		try {
+			String sql = "select IOP.JSASTEPS_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getJSASTOPSerialNo() {
+		try {
+			String sql = "select IOP.JSASTOP_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getPTWREQDOC() {
+		try {
+			String sql = "select IOP.PTWREQDOC_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+//	public String getPTWAPPROVAL() {
+//		try {
+//			String sql = "select IOP.PTWAPPROVAL_SEQ.NEXTVAL FROM DUMMY";
+//			Query q = getSession().createNativeQuery(sql);
+//			return q.getSingleResult().toString();
+//		} catch (Exception e) {
+//			logger.error(e.getMessage());
+//		}
+//		return null;
+//	}
+	
+	public String getPTWTESTREC() {
+		try {
+			String sql = "select IOP.PTWTESTREC_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getPTWATESTRES()  {
+		try {
+			String sql = "select IOP.PTWATESTRES_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getTOPTWCLOSEOUT()  {
+		try {
+			String sql = "select IOP.CLOSE_OUT_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String getTOJSALOCATION()  {
+		try {
+			String sql = "select IOP.JSA_LOCATION_SEQ.NEXTVAL FROM DUMMY";
+			Query q = getSession().createNativeQuery(sql);
+			return q.getSingleResult().toString();
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
