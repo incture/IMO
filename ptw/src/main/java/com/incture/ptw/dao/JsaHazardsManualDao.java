e87bbce9c330d19ed85fdacca0e041d8716fc1b3
inc01389 <krishna.s@incture.com>
2021-04-22 11:22:34 +0530

    latest

--- ptw/src/main/java/com/incture/ptw/dao/JsaHazardsManualDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaHazardsManualDao.java
@@ -1,8 +1,29 @@
 package com.incture.ptw.dao;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
+
+import com.incture.ptw.dto.JsaHazardsManualDto;
 
 @Repository
 public class JsaHazardsManualDao extends BaseDao {
+	public void insertJsaHazardsManual(String permitNumber, JsaHazardsManualDto jsaHazardsManualDto) {
+		try {
+			String sql = "INSERT INTO \"IOP\".\"JSAHAZARDSMANUAL\" VALUES (?,?,?,?,?,?,?)";
+			logger.info(sql);
+			Query query = getSession().createNativeQuery(sql);
+			query.setParameter(1, permitNumber);
+			query.setParameter(2, jsaHazardsManualDto.getManualHandling());
+			query.setParameter(3, jsaHazardsManualDto.getAssessManualTask());
+			query.setParameter(4, jsaHazardsManualDto.getLimitLoadSize());
+			query.setParameter(5, jsaHazardsManualDto.getProperLiftingTechnique());
+			query.setParameter(6, jsaHazardsManualDto.getConfirmStabilityOfLoad());
+			query.setParameter(7, jsaHazardsManualDto.getGetAssistanceOrAid());
+			query.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
 
 }
--- ptw/src/main/java/com/incture/ptw/dao/JsaHazardsToolsDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaHazardsToolsDao.java
@@ -1,8 +1,30 @@
 package com.incture.ptw.dao;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
+import com.incture.ptw.dto.JsaHazardsToolsDto;
+
 @Repository
-public class JsaHazardsToolsDao extends BaseDao{
+public class JsaHazardsToolsDao extends BaseDao {
+	public void insertJsaHazardsTools(String permitNumber, JsaHazardsToolsDto jsaHazardsToolsDto) {
+		try {
+			String sql = "INSERT INTO \"IOP\".\"JSAHAZARDSTOOLS\" VALUES (?,?,?,?,?,?,?,?)";
+			logger.info(sql);
+			Query query = getSession().createNativeQuery(sql);
+			query.setParameter(1, permitNumber);
+			query.setParameter(2, jsaHazardsToolsDto.getEquipmentAndTools());
+			query.setParameter(3, jsaHazardsToolsDto.getInspectEquipmentTool());
+			query.setParameter(4, jsaHazardsToolsDto.getBrassToolsNecessary());
+			query.setParameter(5, jsaHazardsToolsDto.getUseProtectiveGuards());
+			query.setParameter(6, jsaHazardsToolsDto.getUseCorrectTools());
+			query.setParameter(7, jsaHazardsToolsDto.getCheckForSharpEdges());
+			query.setParameter(8, jsaHazardsToolsDto.getApplyHandSafetyPrinciple());
+			query.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
 
 }
