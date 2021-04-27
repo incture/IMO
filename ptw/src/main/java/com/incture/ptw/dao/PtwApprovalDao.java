a96135f9d3f56076e3d976e42695cb9af0a62aae
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-27 13:06:34 +0530

    ptwApprovalDao method done

--- ptw/src/main/java/com/incture/ptw/dao/PtwApprovalDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/PtwApprovalDao.java
@@ -1,23 +1,56 @@
 package com.incture.ptw.dao;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.incture.ptw.dto.PtwApprovalDto;
 
-
 @Repository
-public class PtwApprovalDao extends BaseDao{
+public class PtwApprovalDao extends BaseDao {
 	public List<PtwApprovalDto> getPtwApproval(String permitNumber, String isCwp, String isHwp, String isCse) {
 		try {
-			
+			String sql = "select * from IOP.PTWAPPROVAL where PERMITNUMBER= ? and ISCWP = ? and ISHWP = ? and ISCSE = ?";
+			Query query = getSession().createNativeQuery(sql);
+			query.setParameter(1, permitNumber);
+			query.setParameter(2, isCwp);
+			query.setParameter(3, isHwp);
+			query.setParameter(4, isCse);
+			@SuppressWarnings("unchecked")
+			List<Object[]> list = query.getResultList();
+			List<PtwApprovalDto> ptwRequiredDocumentDtoList = new ArrayList<PtwApprovalDto>();
+			for (Object[] o : list) {
+				PtwApprovalDto ptwApprovalDto = new PtwApprovalDto();
+				ptwApprovalDto.setSerialNo((Integer) o[0]);
+				ptwApprovalDto.setPermitNumber((Integer) o[1]);
+				ptwApprovalDto.setIsCWP(Integer.parseInt(o[2].toString()));
+				ptwApprovalDto.setIsHWP(Integer.parseInt(o[3].toString()));
+				ptwApprovalDto.setIsCSE(Integer.parseInt(o[4].toString()));
+				ptwApprovalDto.setIsWorkSafeToPerform((int) o[5]);
+				ptwApprovalDto.setPreJobWalkThroughBy((String) o[6]);
+				ptwApprovalDto.setApprovedBy((String) o[7]);
+				ptwApprovalDto.setApprovalDate((Date) o[8]);
+				ptwApprovalDto.setControlBoardDistribution((int) o[9]);
+				ptwApprovalDto.setWorkSiteDistribution((int) o[10]);
+				ptwApprovalDto.setSimopsDistribution((int) o[11]);
+				ptwApprovalDto.setOtherDistribution((String) o[12]);
+				ptwApprovalDto.setPicName((String) o[13]);
+				ptwApprovalDto.setPicDate((Date) o[14]);
+				ptwApprovalDto.setSuperItendentName((String) o[15]);
+				ptwApprovalDto.setSuperItendentDate((Date) o[16]);
+				ptwRequiredDocumentDtoList.add(ptwApprovalDto);
+
+			}
+			return ptwRequiredDocumentDtoList;
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
 
 }
