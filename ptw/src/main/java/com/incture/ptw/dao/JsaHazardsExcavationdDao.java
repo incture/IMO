9b49a169463a669cb27a716af7f5dbeee25c4f0f
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-04-29 12:47:59 +0530

    updates

--- ptw/src/main/java/com/incture/ptw/dao/JsaHazardsExcavationdDao.java
+++ ptw/src/main/java/com/incture/ptw/dao/JsaHazardsExcavationdDao.java
@@ -9,7 +9,7 @@ import com.incture.ptw.dto.JsaHazardsExcavationdDto;
 @Repository
 public class JsaHazardsExcavationdDao extends BaseDao {
 
-	public void insertJsaHazardsExcavation(String permitNumber,JsaHazardsExcavationdDto jsaHazardsExcavationdDto) {
+	public void insertJsaHazardsExcavation(String permitNumber, JsaHazardsExcavationdDto jsaHazardsExcavationdDto) {
 		try {
 			logger.info("JsaHazardsExcavationdDto: " + jsaHazardsExcavationdDto);
 			String sql = "INSERT INTO \"IOP\".\"JSAHAZARDSEXCAVATION\" VALUES (?,?,?,?,?,?)";
@@ -20,6 +20,27 @@ public class JsaHazardsExcavationdDao extends BaseDao {
 			query.setParameter(4, jsaHazardsExcavationdDto.getLocatePipesByHandDigging());
 			query.setParameter(5, jsaHazardsExcavationdDto.getDeEnergizeUnderground());
 			query.setParameter(6, jsaHazardsExcavationdDto.getCseControls());
+			logger.info("sql " + sql);
+			query.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+
+		}
+
+	}
+
+	public void updateJsaHazardsExcavation(JsaHazardsExcavationdDto jsaHazardsExcavationdDto) {
+		try {
+			logger.info("JsaHazardsExcavationdDto: " + jsaHazardsExcavationdDto);
+			String sql = "UPDATE \"IOP\".\"JSAHAZARDSEXCAVATION\" SET  \"EXCAVATIONS\"=?,\"HAVEEXCAVATIONPLAN\"=?,\"LOCATEPIPESBYHANDDIGGING\"=?,"
+					+ "\"DEENERGIZEUNDERGROUND\"=?,\"CSECONTROLS\"=? WHERE \"PERMITNUMBER\"=?";
+			Query query = getSession().createNativeQuery(sql);
+			query.setParameter(1, jsaHazardsExcavationdDto.getExcavations());
+			query.setParameter(2, jsaHazardsExcavationdDto.getHaveExcavationPlan());
+			query.setParameter(3, jsaHazardsExcavationdDto.getLocatePipesByHandDigging());
+			query.setParameter(4, jsaHazardsExcavationdDto.getDeEnergizeUnderground());
+			query.setParameter(5, jsaHazardsExcavationdDto.getCseControls());
+			query.setParameter(6, jsaHazardsExcavationdDto.getPermitNumber());
 			logger.info("sql " + sql);
 			query.executeUpdate();
 		} catch (Exception e) {
