97498233b5224fb6565c2e4f665f21f9ae2f7de6
Durgesh.Patidar <Durgesh.Patidar@INC01350>
2021-06-08 14:34:01 +0530

    createtemplatedto added

--- iop-ptw/src/main/java/com/incture/iopptw/template/dtos/CreateTemplateDto.java
+++ iop-ptw/src/main/java/com/incture/iopptw/template/dtos/CreateTemplateDto.java
@@ -1,5 +1,28 @@
 package com.incture.iopptw.template.dtos;
 
+import com.incture.iopptw.dtos.JsaHazardsDroppedDto;
+import com.incture.iopptw.dtos.JsaHazardsElectricalDto;
+import com.incture.iopptw.dtos.JsaHazardsExcavationdDto;
+import com.incture.iopptw.dtos.JsaHazardsFallsDto;
+import com.incture.iopptw.dtos.JsaHazardsHeightsDto;
+import com.incture.iopptw.dtos.JsaHazardsHighNoiseDto;
+import com.incture.iopptw.dtos.JsaHazardsIgnitionDto;
+import com.incture.iopptw.dtos.JsaHazardsLiftingDto;
+import com.incture.iopptw.dtos.JsaHazardsManualDto;
+import com.incture.iopptw.dtos.JsaHazardsMobileDto;
+import com.incture.iopptw.dtos.JsaHazardsMovingDto;
+import com.incture.iopptw.dtos.JsaHazardsPersonnelDto;
+import com.incture.iopptw.dtos.JsaHazardsPressurizedDto;
+import com.incture.iopptw.dtos.JsaHazardsSimultaneousDto;
+import com.incture.iopptw.dtos.JsaHazardsSpillsDto;
+import com.incture.iopptw.dtos.JsaHazardsSubstancesDto;
+import com.incture.iopptw.dtos.JsaHazardsToolsDto;
+import com.incture.iopptw.dtos.JsaHazardsVisibilityDto;
+import com.incture.iopptw.dtos.JsaHazardsVoltageDto;
+import com.incture.iopptw.dtos.JsaHazardsWeatherDto;
+import com.incture.iopptw.dtos.JsaHazardscseDto;
+import com.incture.iopptw.dtos.JsaStepsDto;
+import com.incture.iopptw.dtos.JsaStopTriggerDto;
 import com.incture.iopptw.dtos.JsaheaderDto;
 import com.incture.iopptw.dtos.JsappeDto;
 
@@ -9,4 +32,27 @@ import lombok.Data;
 public class CreateTemplateDto {
 	private JsaheaderDto jsaheaderDto;
 	private JsappeDto jsappeDto;
+	private JsaStepsDto jsaStepsDto;
+	private JsaHazardsPressurizedDto jsaHazardsPressurizedDto;
+	private JsaHazardsVisibilityDto jsaHazardsVisibilityDto;
+	private JsaHazardsPersonnelDto jsaHazardsPersonnelDto;
+	private JsaHazardscseDto jsaHazardscseDto;
+	private JsaHazardsSimultaneousDto jsaHazardsSimultaneousDto;
+	private JsaHazardsIgnitionDto jsaHazardsIgnitionDto;
+	private JsaHazardsSubstancesDto jsaHazardsSubstancesDto;
+	private JsaHazardsSpillsDto jsaHazardsSpillsDto;
+	private JsaHazardsWeatherDto jsaHazardsWeatherDto;
+	private JsaHazardsHighNoiseDto jsaHazardsHighNoiseDto;
+	private JsaHazardsDroppedDto jsaHazardsDroppedDto;
+	private JsaHazardsLiftingDto jsaHazardsLiftingDto;
+	private JsaHazardsHeightsDto jsaHazardsHeightsDto;
+	private JsaHazardsElectricalDto jsaHazardsElectricalDto;
+	private JsaHazardsMovingDto jsaHazardsMovingDto;
+	private JsaHazardsManualDto jsaHazardsManualDto;
+	private JsaHazardsToolsDto jsaHazardsToolsDto;
+	private JsaHazardsFallsDto jsaHazardsFallsDto;
+	private JsaHazardsVoltageDto jsaHazardsVoltageDto;
+	private JsaHazardsExcavationdDto jsaHazardsExcavationdDto;
+	private JsaHazardsMobileDto jsaHazardsMobileDto;
+	private JsaStopTriggerDto jsaStopTriggerDto;
 }
